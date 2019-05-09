ad4b781491a3d01daa274345f2ed0d6d22903b59
Leon Chen <leonc@synnex.org>
2019-05-09 19:03:41 +0800

    Google

--- src/main/resources/Chromium/qgenLogin/background.js
+++ src/main/resources/Chromium/qgenLogin/background.js
@@ -1,15 +1,6 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-'use strict';
-
-// Simple extension to remove 'Cookie' request header and 'Set-Cookie' response
-// header.
-
 function removeHeader(headers, name) {
     for (var i = 0; i < headers.length; i++) {
-        if (headers[i].name.toLowerCase() == name) {
+        if (headers[i].name.toLowerCase() === name) {
             console.log('Removing "' + name + '" header.');
             headers.splice(i, 1);
             break;
@@ -17,22 +8,27 @@ function removeHeader(headers, name) {
     }
 }
 
+function removeOneCookie(url, name) {
+    chrome.cookies.remove({url: url, name: name}, function (details) {
+        console.log(details.name);
+    });
+}
+
+function removeAllCookies(url) {
+    chrome.cookies.getAll({url: url}, function (cookies) {
+        for (var i = 0; i < cookies.length; i++) {
+            removeOneCookie(url, cookies[i].name);
+        }
+    });
+}
+
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function (details) {
-        removeHeader(details.requestHeaders, 'cookie');
+        removeAllCookies("https://www.baidu.com/");
+        // removeHeader(details.requestHeaders, 'cookie');
         return {requestHeaders: details.requestHeaders};
     },
     // filters
     {urls: ['https://*/*', 'http://*/*']},
     // extraInfoSpec
-    ['blocking', 'requestHeaders', 'extraHeaders']);
-
-chrome.webRequest.onHeadersReceived.addListener(
-    function (details) {
-        removeHeader(details.responseHeaders, 'set-cookie');
-        return {responseHeaders: details.responseHeaders};
-    },
-    // filters
-    {urls: ['https://*/*', 'http://*/*']},
-    // extraInfoSpec
-    ['blocking', 'responseHeaders', 'extraHeaders']);
+    ['requestHeaders']);
