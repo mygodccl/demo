02f43422d73a579017d1d1fa5e3b1e87108b9e6e
Leon Chen <leonc@synnex.com>
2019-05-09 18:24:12 +0800

    orika

--- src/test/java/com/example/demo/MainTest.java
+++ src/test/java/com/example/demo/MainTest.java
@@ -1,56 +1,27 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
 
 import com.example.demo.orika.BO.PersonDestination;
 import com.example.demo.orika.entity.PersonSource;
-import com.example.demo.util.sift.ResultMap;
-import com.example.demo.util.sift.SiftUtil;
+import com.example.demo.util.orika.mapper.Boolean2StringMapper;
 
+import ma.glasnost.orika.BoundMapperFacade;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
-import ma.glasnost.orika.metadata.ScoringClassMapBuilder;
 
 public class MainTest {
-    MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
+    MapperFactory mapperFactory = new DefaultMapperFactory.Builder().mapNulls(false).useBuiltinConverters(true).build();
 
     @Test
     public void simple() {
-        MapperFactory factory = new DefaultMapperFactory.Builder().classMapBuilderFactory(new ScoringClassMapBuilder.Factory()).build();
-    }
+        mapperFactory.classMap(PersonSource.class, PersonDestination.class).field("active1", "active2").byDefault()
+                        .customize(new Boolean2StringMapper(mapperFactory)).register();
 
-    @Test
-    public void sift() {
-        List<PersonSource> left = new ArrayList<>();
-        PersonSource a = new PersonSource(1, "a");
-        PersonSource b = new PersonSource(2, "b");
-        PersonSource c = new PersonSource(3, "c");
-        PersonSource g = new PersonSource(7, "g");
-        PersonSource k = new PersonSource(8, "k");
-        left.add(b);
-        left.add(c);
-        left.add(a);
-        left.add(g);
-        left.add(k);
-
-        List<PersonDestination> right = new ArrayList<>();
-        PersonDestination a1 = new PersonDestination(1, "a");
-        PersonDestination b1 = new PersonDestination(2, "b");
-        PersonDestination c1 = new PersonDestination(3, "c");
-        right.add(a1);
-        right.add(b1);
-        right.add(c1);
-        PersonDestination e = new PersonDestination(4, "e");
-        PersonDestination f = new PersonDestination(5, "f");
-        right.add(e);
-        right.add(f);
-
-        SiftUtil.Processor<PersonSource, PersonDestination> processor = SiftUtil.buildProcess(left, right);
-        ResultMap<PersonSource, PersonDestination> resultMap = processor.process((l, r) -> l.getId().equals(r.getId()));
-
-        System.out.println(1);
+        BoundMapperFacade<PersonSource, PersonDestination> facade = mapperFactory.getMapperFacade(PersonSource.class, PersonDestination.class);
+        PersonDestination destination = facade.map(new PersonSource(1, "ccl", true));
+        PersonSource personSource = facade.mapReverse(new PersonDestination(2, "dy", "Y"));
+        System.out.println(destination);
+        System.out.println(personSource);
     }
 }
--- src/test/java/com/example/demo/orika/entity/PersonSource.java
+++ src/test/java/com/example/demo/orika/entity/PersonSource.java
@@ -2,10 +2,15 @@ package com.example.demo.orika.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PersonSource {
     private Integer id;
     private String name;
+    private Boolean active1;
+    // private Boolean flag;
+    // private Boolean reverse;
 }
